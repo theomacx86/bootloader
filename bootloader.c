e10618873942366db3d86c6a75750b8475807e44
Theo Jehl <theojehl76@gmail.com>
2026-05-08 00:30:58 +0200

    feat: initial commit

---
+++ bootloader.c
@@ -0,0 +1,14 @@
+#include "Uefi.h"
+
+EFI_STATUS
+EFIAPI
+UefiMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  UINT32  Index;
+  Index = 0;
+  // test
+  return Index;
+}
